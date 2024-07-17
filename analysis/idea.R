29f91b14f0554ae68bfee4d370103062f7147b0a
LeoFeatherstone <leofeatherstone@protonmail.com>
2024-07-17 13:44:46 +1000

    update plane fig

--- analysis/idea.R
+++ analysis/idea.R
@@ -12,12 +12,12 @@ pow_7 <- function(x) exp(7)
 
 # write data manually for now
 data <- data.frame(
-    "rate" = c(0.004, 0.001, 0.0000006, 0.00000001),
-    "g_length" = c(13158, 29903, 4825265, 4300000),
+    "rate" = c(0.004, 0.001, 0.000001, 0.00000001),
+    "g_length" = c(13158, 29903, 2900000, 4300000),
     "name" = c(
         "H1N1",
         "SARS-CoV-2",
-        "italic(S.~sonnei)",
+        "italic(S.~aureus)",
         "italic(M.~tuberculosis)"
     )
 )
@@ -29,16 +29,23 @@ data <- cbind.data.frame(data, "resolution" = rep(c(7, 30, 365.25)))
 
 data <- data %>%
     mutate(m_t = log_7(m_t)) %>%
-    mutate(resolution = log_7(resolution))
+    mutate(resolution = log_7(resolution)) %>%
+    mutate(col = case_when(
+        !(name %in% c(
+            "H1N1", "SARS-CoV-2",
+            "italic(S.~aureus)", "italic(M.~tuberculosis)"
+        )) ~ "dodgerblue",
+        .default = "coral"
+    ))
 
 
 t_breaks <- sapply(c(1, 30, 365.25), function(x) log_7(x))
 
 data %>%
-    ggplot(aes(x = resolution, y = m_t, group = name, label = name)) +
+    ggplot(aes(x = resolution, y = m_t, group = name, label = name, fill = col)) +
     annotate("polygon", x = c(0, 0, 4.8), y = c(0, 4.8, 4.8), fill = "grey") +
     geom_line() +
-    geom_point(pch = 21, size = 7, fill = alpha("red", 0.7)) +
+    geom_point(pch = 21, size = 7) +
     geom_text(
         data = subset(data, resolution == 1),
         aes(x = resolution, y = m_t, label = name),
@@ -46,7 +53,8 @@ data %>%
         nudge_y = 0.1,
         nudge_x = -0.05,
         size = 6,
-        hjust = 0
+        hjust = 0,
+        check_overlap = TRUE
     ) +
     geom_abline(intercept = 1e-100, slope = 1) +
     coord_cartesian(xlim = c(0, 4.7), ylim = c(0, 4.7)) +
@@ -79,10 +87,12 @@ data %>%
     theme(
         panel.grid = element_blank(),
         axis.text = element_text(size = 14),
-        axis.title = element_text(size = 14)
+        axis.title = element_text(size = 14),
+        legend.position = "none",
+        text = element_text(size = 14)
     )
 ggsave(
-    "plane.pdf",
+    "figures/plane.pdf",
     dpi = 300,
     units = "in"
 )
