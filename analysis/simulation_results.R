eecdc82926bd21ff4ca627e1cdf0e4099920f025
LeoFeatherstone <leofeatherstone@protonmail.com>
2024-12-10 13:59:28 +1100

    remove old figs, add simulation variance fig

--- analysis/simulation_results.R
+++ analysis/simulation_results.R
@@ -10,7 +10,7 @@ library(htmlwidgets)
 ## TODO: Update path later
 trace <- readRDS("analysis/processed_simulation_posteriors/posteriors.RData")
 ess <- readRDS("analysis/processed_simulation_posteriors/ess.RData")
-tree_stats <- readRDS("analysis/processed_simulation_posteriors/tree_imbalance.RData")
+#tree_stats <- readRDS("analysis/processed_simulation_posteriors/tree_imbalance.RData")
 
 # ess 50% gave the best results (ess[[9]])
 ess_passed <- names(which(unlist(
@@ -64,40 +64,40 @@ posterior$factor <- factor(
 )
 
 ### Handle Tree Stats
-tree_stats <- tree_stats %>%
-    mutate(
-        id = paste(
-            organism, treePrior, clockodel,
-            resolution, replicate,
-            sep = "_"
-        )
-    ) %>%
-    filter(id %in% ess_passed) %>%
-    ## REVERSING MISTAKES, I will be able to delete the next chunk when I rerun
-    rename(
-        "treePrior" = treePrior,
-        "clockModel" = clockodel
-    ) %>%
-    mutate(
-        resolution = case_when(
-            resolution == "Day-Fixed" ~ "Year",
-            resolution == "Month-Fixed" ~ "Month",
-            resolution == "Year-Fixed" ~ "Day"
-        )
-    )
+# tree_stats <- tree_stats %>%
+#     mutate(
+#         id = paste(
+#             organism, treePrior, clockodel,
+#             resolution, replicate,
+#             sep = "_"
+#         )
+#     ) %>%
+#     filter(id %in% ess_passed) %>%
+#     ## REVERSING MISTAKES, I will be able to delete the next chunk when I rerun
+#     rename(
+#         "treePrior" = treePrior,
+#         "clockModel" = clockodel
+#     ) %>%
+#     mutate(
+#         resolution = case_when(
+#             resolution == "Day-Fixed" ~ "Year",
+#             resolution == "Month-Fixed" ~ "Month",
+#             resolution == "Year-Fixed" ~ "Day"
+#         )
+#     )
 
-## Making species a factor to plot with
-tree_stats$factor <- factor(
-    levels = c("h1n1", "sars-cov-2", "saureus", "tb"),
-    tree_stats$organism,
-    labels = c(
-        "H1N1", "SARS-CoV-2",
-        "italic(S.~aureus)", "italic(M.~tuberculosis)"
-    )
-)
-## Filter cols for binding later
-tree_stats <- tree_stats %>%
-    select(!c(organism, id))
+# ## Making species a factor to plot with
+# tree_stats$factor <- factor(
+#     levels = c("h1n1", "sars-cov-2", "saureus", "tb"),
+#     tree_stats$organism,
+#     labels = c(
+#         "H1N1", "SARS-CoV-2",
+#         "italic(S.~aureus)", "italic(M.~tuberculosis)"
+#     )
+# )
+# ## Filter cols for binding later
+# tree_stats <- tree_stats %>%
+#     select(!c(organism, id))
 
 ## Simulation values to compare against
 rate_true <- data.frame(
@@ -140,63 +140,6 @@ reproductive_number_true <- data.frame(
         "mean_Re1", "mean_Re2", "mean_Re1", "mean_Re2"
     )
 )
-
-rel_error <- posterior %>%
-    ungroup() %>%
-    select(!organism) %>%
-    group_by(factor, treePrior, resolution, replicate) %>%
-    summarise(
-        mean_R0 = mean(reproductiveNumber),
-        mean_Re1 = mean(reproductiveNumber.1),
-        mean_Re2 = mean(reproductiveNumber.2),
-        mean_age = mean(Tree.height),
-        mean_clock_rate = mean(clockRate),
-        .groups = "keep"
-    ) %>%
-    pivot_wider(
-        names_from = resolution,
-        names_glue = "{.value}_{resolution}",
-        values_from = starts_with("mean_")
-    ) %>%
-    mutate(
-        #R0
-        error_R0_Day = 0,
-        error_R0_Month = mean_R0_Month - mean_R0_Day,
-        error_R0_Year = mean_R0_Year - mean_R0_Day,
-        #Re1
-        error_Re1_Day = 0,
-        error_Re1_Month = mean_Re1_Month - mean_Re1_Day,
-        error_Re1_Year = mean_Re1_Year - mean_Re1_Day,
-        #Re2
-        error_Re2_Day = 0,
-        error_Re2_Month = mean_Re2_Month - mean_Re2_Day,
-        error_Re2_Year = mean_Re2_Year - mean_Re2_Day,
-        #age
-        error_age_Day = 0,
-        error_age_Month = mean_age_Month - mean_age_Day,
-        error_age_Year = mean_age_Year - mean_age_Day,
-        #rate
-        error_clock_rate_Day = 0,
-        error_clock_rate_Month = mean_clock_rate_Month - mean_clock_rate_Day,
-        error_clock_rate_Year = mean_clock_rate_Year - mean_clock_rate_Day,
-    ) %>%
-    select(!starts_with("mean_")) %>%
-    pivot_longer(
-        starts_with("error_"),
-        names_to = c(".value", "resolution"),
-        names_pattern = "(error_.+)_(Day|Month|Year)"
-    ) %>%
-    pivot_longer(
-            matches("error_R(0|e1|e2)"),
-            names_to = "interval",
-            values_to = "error_reproductive_number",
-            values_drop_na = TRUE
-    ) %>%
-    mutate(interval = sub(interval, pattern = "error_", replacement = "")) %>%
-    left_join(
-        tree_stats,
-        by = c("factor", "replicate", "resolution", "treePrior")
-    )
 
 ####################################
 ############# Plotting #############
@@ -254,11 +197,8 @@ clock_plot <- (posterior %>%
         panel.grid.minor = element_blank(),
         text = element_text(size = 14)
     ))
-clock_plot
-ggsave("figures/clock_traj.pdf", dpi = 300, width = 10, height = 5, units = "in")
 
 ## Origin Parallel Coords
-
 origin_plot <- (posterior %>%
     group_by(factor, treePrior, resolution, replicate) %>%
     summarise(mean_outbreak_age = mean(Tree.height)) %>%
@@ -311,22 +251,8 @@ origin_plot <- (posterior %>%
         panel.grid.minor = element_blank(),
         text = element_text(size = 14)
     ))
-origin_plot
-ggsave("figures/origin_traj.pdf", dpi = 300, width = 10, height = 5, units = "in")
-
-
-
 
 ## Reproductive Number Parallel Coords
-reproductive_number_true$factor <- factor(
-    reproductive_number_true$organism,
-    levels = c("h1n1", "sars-cov-2", "saureus", "tb"),
-        labels = c(
-            "H1N1", "SARS-CoV-2",
-            "italic(S.~aureus)", "italic(M.~tuberculosis)"
-        )
-    )
-
 reproductive_number_plot <- (posterior %>%
     filter(
         !(
@@ -407,11 +333,7 @@ reproductive_number_plot <- (posterior %>%
         text = element_text(size = 14),
         legend.position = "bottom",
         panel.grid.minor = element_blank()
-    ))
-reproductive_number_plot
-ggsave(
-    "figures/reproductive_number_traj.pdf", dpi = 300,
-    width = 10, height = 5, units = "in"
+    )
 )
 
 ## Simulation Combined Parms plot
@@ -426,7 +348,152 @@ ggsave(
     dpi = 300
 )
 
-## Likelihood plots. TODO Make interactive later
+### Variance plots - repeat above with variance instead of mean
+variance_data <- posterior %>%
+    group_by(factor, treePrior, resolution, replicate) %>%
+    summarise(
+        var_R0 = var(reproductiveNumber),
+        var_Re1 = var(reproductiveNumber.1),
+        var_Re2 = var(reproductiveNumber.2),
+        var_outbreak_age = var(Tree.height),
+        var_clock_rate = var(clockRate)
+    ) %>%
+    pivot_longer(
+            matches("var_R0|var_Re1|var_Re2"),
+            names_to = "interval",
+            values_to = "var_reproductive_number",
+            values_drop_na = TRUE
+    ) %>%
+    filter(
+        !(factor == "H1N1" & resolution == "Year")
+        &
+        !(factor == "SARS-CoV-2" & resolution == "Year")
+    ) 
+
+## Origin Error vs Variance
+origin_variance <- variance_data %>%
+    ggplot(aes(x = resolution, y = var_outbreak_age)) +
+        geom_line(
+            aes(
+                group = interaction(factor, replicate, treePrior),
+                col = treePrior
+            ),
+            alpha = 0.5
+        ) +
+        geom_boxplot(
+        aes(
+            fill = treePrior
+        ),
+        position = "dodge", alpha = 0.3, outlier.shape = NA
+    ) +
+        facet_wrap(~factor, scales = "free", labeller = label_parsed, ncol = 4) +
+        xlab("Date Resolution") +
+        ylab("Variance in tMRCA") +
+            scale_discrete_manual(
+        aesthetics = c("col", "fill"),
+        labels = c("Birth Death", "Coalescent Exponential"),
+        values = c("dodgerblue", "darkorange")
+    ) +
+        theme_bw() +
+        theme(
+            legend.title = element_blank(),
+            legend.position = "bottom",
+            text = element_text(size = 14),
+            panel.grid.minor = element_blank()
+        )
+
+## Clock Rate Error vs Variance
+clock_variance <- variance_data %>%
+    ggplot(aes(x = resolution, y = var_clock_rate, col = treePrior)) +
+        geom_line(
+            aes(
+                group = interaction(factor, replicate, treePrior),
+                col = treePrior
+            ),
+            alpha = 0.5
+        ) +
+            geom_boxplot(
+        aes(
+            fill = treePrior
+        ),
+        position = "dodge", alpha = 0.3, outlier.shape = NA
+    ) +
+        facet_wrap(~factor, scales = "free", labeller = label_parsed, ncol = 4) +
+        xlab("Date Resolution") +
+        ylab("Variance in substitution rate") +
+            scale_discrete_manual(
+        aesthetics = c("col", "fill"),
+        labels = c("Birth Death", "Coalescent Exponential"),
+        values = c("dodgerblue", "darkorange")
+    ) +
+        theme_bw() +
+        theme(
+            legend.title = element_blank(),
+            legend.position = "none",
+            text = element_text(size = 14),
+            panel.grid.minor = element_blank()
+        )
+
+## Reproductive Number Error vs Variance
+reproductive_number_variance <- variance_data %>%
+    ggplot(aes(x = resolution, y = var_reproductive_number, col = interaction(interval, treePrior))) +
+    geom_line(
+            aes(
+                group = interaction(factor, interval, replicate, treePrior),
+                col = interaction(interval, treePrior)
+            ),
+            alpha = 0.5
+        ) +
+        geom_boxplot(
+        aes(
+            x = resolution, y = var_reproductive_number,
+            group = interaction(interval, resolution, treePrior),
+            fill = interaction(interval, treePrior)
+        ),
+        position = "dodge", alpha = 0.3,
+        outlier.shape = NA
+    ) +
+        facet_wrap(~factor, scales = "free", labeller = label_parsed, ncol = 4) +
+        xlab("Date Resolution") +
+        ylab(latex2exp::TeX("Variance in $\\textit{R_{\\bullet}}$")) +
+        scale_discrete_manual(
+        aesthetics = c("col", "fill"),
+        labels = c(
+            ~italic(R[0])~Birth~Death, ~italic(R[0])~Coalescent~Exponential,
+            ~italic(R[e[1]])~Birth~Death, ~italic(R[e[2]])~Birth~Death
+         ),
+         values = c(
+            "var_R0.BD" = "dodgerblue", "var_Re1.BD" = "purple",
+            "var_Re2.BD" = "green", "var_R0.CE" = "darkorange"
+        ),
+        limits = c(
+            "var_R0.BD", "var_R0.CE",
+            "var_Re1.BD", "var_Re2.BD"
+        )
+    ) +
+        theme_bw() +
+        theme(
+            legend.title = element_blank(),
+            legend.position = "bottom",
+            text = element_text(size = 14),
+            panel.grid.minor = element_blank()
+        )
+
+
+## Simulation Combined Parms plot
+var_plot <- cowplot::plot_grid(
+    labels = "AUTO",
+    clock_variance, origin_variance, reproductive_number_variance,
+    align = "v",
+    nrow = 3
+)
+ggsave(
+    plot = var_plot,
+    file = "figures/simulation_variance_panel.pdf",
+    dpi = 300
+)
+
+### Likelihood plots. TODO Make interactive later
 sim_likielihood_plot <- posterior %>%
     pivot_longer(
         matches("BDSKY_Serial|CoalescentExponential"),
@@ -496,137 +563,37 @@ ggsave(
 )
 
 ### EI branch ratio vs resolution
-ei_plot <- (rel_error %>%
-    filter(
-        !(
-            (factor == "H1N1" | factor == "SARS-CoV-2")
-            & resolution == "Year"
-        )
-    ) %>%
-    group_by(factor, treePrior, resolution, replicate) %>%
-    ggplot() +
-    geom_line(
-        aes(
-            x = resolution,
-            y = ei_ratio,
-            group = interaction(factor, replicate, treePrior),
-            col = interaction(treePrior)
-        ),
-        alpha = 1, linewidth = 0.5
-    ) +
-    facet_wrap(
-        ~factor, nrow = 1,
-        scales = "free",
-        labeller = label_parsed
-    ) +
-    ylab("External to Internal Branch Length Ratio") +
-    xlab("Date resolution") +
-    theme_bw() +
-    theme(
-        legend.title = element_blank(),
-        legend.position = "bottom",
-        panel.grid.minor = element_blank()
-    ))
-ei_plot
+## Commented out b/c result not informative 09-12-24
+# ei_plot <- (rel_error %>%
+#     filter(
+#         !(
+#             (factor == "H1N1" | factor == "SARS-CoV-2")
+#             & resolution == "Year"
+#         )
+#     ) %>%
+#     group_by(factor, treePrior, resolution, replicate) %>%
+#     ggplot() +
+#     geom_line(
+#         aes(
+#             x = resolution,
+#             y = ei_ratio,
+#             group = interaction(factor, replicate, treePrior),
+#             col = interaction(treePrior)
+#         ),
+#         alpha = 1, linewidth = 0.5
+#     ) +
+#     facet_wrap(
+#         ~factor, nrow = 1,
+#         scales = "free",
+#         labeller = label_parsed
+#     ) +
+#     ylab("External to Internal Branch Length Ratio") +
+#     xlab("Date resolution") +
+#     theme_bw() +
+#     theme(
+#         legend.title = element_blank(),
+#         legend.position = "bottom",
+#         panel.grid.minor = element_blank()
+#     ))
+# ei_plot
 ggsave("figures/ei_ratio.pdf", dpi = 300)
-
-### Error plots
-
-## Age error
-rel_error %>%
-    filter(
-        resolution != "Day"
-        &
-        !(factor == "H1N1" & resolution == "Year")
-        &
-        !(factor == "SARS-CoV-2" & resolution == "Year")
-    ) %>%
-    pivot_longer(
-            matches("n_tips|coless_imbalance|sampling_span"),
-            names_to = "stat",
-            values_to = "value",
-            values_drop_na = TRUE
-    ) %>%
-    ggplot(
-        aes(
-            x = value, y = error_age,
-            fill = interaction(interval, treePrior, resolution),
-            col = interaction(interval, treePrior, resolution)
-        )
-    ) +
-    geom_smooth(method = "lm", se = FALSE) +
-    geom_point(shape = 21, alpha = 0.2, col = "black") +
-    facet_wrap(
-        ~ factor * stat, labeller = label_parsed,
-        ncol = 3, scales = "free"
-    ) +
-    theme(
-        legend.position = "bottom", legend.title = element_blank()
-    )
-ggsave("figures/age_error.pdf", dpi = 300)
-
-## clock error
-rel_error %>%
-    filter(
-        resolution != "Day"
-        &
-        !(factor == "H1N1" & resolution == "Year")
-        &
-        !(factor == "SARS-CoV-2" & resolution == "Year")
-    ) %>%
-    pivot_longer(
-            matches("n_tips|coless_imbalance|sampling_span"),
-            names_to = "stat",
-            values_to = "value",
-            values_drop_na = TRUE
-    ) %>%
-    ggplot(
-        aes(
-            x = value, y = error_clock_rate,
-            fill = interaction(interval, treePrior, resolution),
-            col = interaction(interval, treePrior, resolution)
-        )
-    ) +
-    geom_smooth(method = "lm", se = FALSE) +
-    geom_point(shape = 21, alpha = 0.2, col = "black") +
-    facet_wrap(
-        ~ factor * stat, labeller = label_parsed,
-        ncol = 3, scales = "free"
-    ) +
-    theme(
-        legend.position = "bottom", legend.title = element_blank()
-    )
-ggsave("figures/clock_rate_error.pdf", dpi = 300)
-
-## reproductive number error
-rel_error %>%
-    filter(
-        resolution != "Day"
-        &
-        !(factor == "H1N1" & resolution == "Year")
-        &
-        !(factor == "SARS-CoV-2" & resolution == "Year")
-    ) %>%
-    pivot_longer(
-            matches("n_tips|coless_imbalance|sampling_span"),
-            names_to = "stat",
-            values_to = "value",
-            values_drop_na = TRUE
-    ) %>%
-    ggplot(
-        aes(
-            x = value, y = error_reproductive_number,
-            fill = interaction(interval, treePrior, resolution),
-            col = interaction(interval, treePrior, resolution)
-        )
-    ) +
-    geom_smooth(method = "lm", se = FALSE) +
-    geom_point(shape = 21, alpha = 0.2, col = "black") +
-    facet_wrap(
-        ~ factor * stat, labeller = label_parsed,
-        ncol = 3, scales = "free"
-    ) +
-    theme(
-        legend.position = "bottom", legend.title = element_blank()
-    )
-ggsave("figures/reproductie_number_error.pdf", dpi = 300)
